13c190f50c2cd85ab67c9c1d39cf15681da87db0
Xiaotong Lin <xiaotong@Jessicas-MBP-81.hsd1.mi.comcast.net>
2023-11-12 12:35:55 -0500

    code starter - js-tricks

---
+++ outcomes-js-tricky-parts/curried-add.test.js
@@ -0,0 +1,26 @@
+const { curriedAdd } = require("./curried-add");
+describe('curried-add', function () {
+  it('should exist', function () {
+    expect(typeof curriedAdd).toBe(
+      'function',
+      "did you remember to define the  function?");
+  });
+});
+
+// describe("curriedAdd", function() {
+//   it("returns 0 when called with no arguments", function() {
+//     expect(curriedAdd()).toBe(0);
+//   });
+
+//   it("returns a function when called with an argument", function() {
+//     expect(curriedAdd(3)).toBeInstanceOf(Function);
+//   });
+
+//   it("accumulates a total with repeated function calls", function() {
+//     expect(curriedAdd(1)()).toBe(1);
+//     expect(curriedAdd(1)(2)()).toBe(3);
+//     expect(curriedAdd(1)(2)(3)()).toBe(6);
+//     expect(curriedAdd(1)(2)(3)(4)()).toBe(10);
+//     expect(curriedAdd(1)(2)(3)(4)(-10)()).toBe(0);
+//   });
+// });
